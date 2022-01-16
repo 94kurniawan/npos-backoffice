3c589f3da89261f8887705fc0350b1556f9abb97
BK <DilaBK@macBook>
2022-01-16 22:30:33 +0700

    can edit name category and variants

---
+++ src/views/components/ModalAddVariant.vue
@@ -0,0 +1,105 @@
+<template>
+  <div
+    @click="closeModalWindow()"
+    class="
+      hidden
+      fixed
+      inset-0
+      bg-gray-600 bg-opacity-50
+      overflow-y-auto
+      h-full
+      w-full
+    "
+    id="modal-add-variant"
+  >
+    <!--modal content-->
+    <div
+      class="
+        relative
+        mx-auto
+        top-16
+        border
+        md:w-2/6
+        w-5/6
+        h-auto
+        shadow-lg
+        rounded-md
+        bg-white
+        overflow-y-auto
+      "
+    >
+      <div class="mt-3">
+        <div class="flex items-center justify-between gap-2 p-3">
+          <label>Nama Variant</label>
+          <input
+            @keyup.enter="saveVariant()"
+            v-model="variant.name"
+            class="border rounded p-3"
+            type="text"
+          />
+        </div>
+        <div class="flex items-center justify-between gap-2 p-3">
+          <label>SKU</label>
+          <input
+            v-model="variant.sku"
+            class="border rounded p-3"
+            type="text"
+            placeholder="Boleh Kosong"
+          />
+        </div>
+        <div class="items-center px-4 py-3">
+          <button
+            @click="saveVariant()"
+            class="
+              px-4
+              py-4
+              bg-modern-green
+              text-white text-base
+              font-medium
+              rounded-md
+              w-full
+              shadow-sm
+              hover:bg-green-600
+              focus:outline-none focus:ring-2 focus:ring-green-300
+            "
+          >
+            Tambah Variant
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalAddVariant",
+  components: {},
+
+  data() {
+    return {
+      variant: {
+        name: "",
+        sku: "",
+      },
+    };
+  },
+
+  methods: {
+    closeModalWindow() {
+      let modal = document.getElementById("modal-add-variant");
+      if (window.event.target == modal) {
+        modal.style.display = "none";
+      }
+    },
+    closeModal() {
+      let modal = document.getElementById("modal-add-variant");
+      modal.style.display = "none";
+    },
+    saveVariant() {
+      this.$emit("add-new-variant", this.variant);
+      this.closeModal();
+    },
+  },
+};
+</script>
--- src/views/store_items/EditItem.vue
+++ src/views/store_items/EditItem.vue
@@ -5,7 +5,7 @@
         <div class="sticky top-0 bg-modern-green text-white">
           <p class="text-center uppercase p-3 md:mb-3 font-bold">Edit Item</p>
         </div>
-        <div class="py-3 px-2 grid grid-flow-row grid-cols-1 md:grid-cols-2">
+        <div class="p-2 grid grid-flow-row grid-cols-1 md:grid-cols-2">
           <p class="py-2 text-sm text-gray-500">Item Name</p>
           <input
             v-model="item.name"
@@ -13,6 +13,173 @@
             name="itemName"
             class="p-3 border rounded"
           />
+        </div>
+        <div class="p-2 grid grid-flow-row grid-cols-1 md:grid-cols-2">
+          <p class="py-2 text-sm text-gray-500">Category</p>
+          <select v-model="item.store_category_id" class="p-3 border rounded">
+            <option
+              v-for="category in categories"
+              :key="category.key"
+              :value="category.id"
+            >
+              {{ category.name }}
+            </option>
+            <!-- <option value="">test</option> -->
+          </select>
+        </div>
+        <div
+          v-if="item.have_variants == true"
+          class="p-2 grid grid-flow-row grid-cols-1 md:grid-cols-2"
+        >
+          <p class="py-2 text-sm text-gray-500">Variants</p>
+          <div class="grid grid-flow-row grid-cols-3 gap-2">
+            <div
+              @click="showAddNewVariant()"
+              class="
+                border-2
+                p-3
+                align-middle
+                bg-gray-500
+                text-white
+                rounded-md
+                active:bg-gray-200
+                justify-center
+                flex
+                items-center
+              "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4v16m8-8H4"
+                />
+              </svg>
+            </div>
+            <div
+              v-for="variant in item.variants"
+              :key="variant.key"
+              class="
+                border-2
+                p-3
+                align-middle
+                bg-gray-50
+                rounded-md
+                active:bg-gray-200
+                text-center
+              "
+            >
+              {{ variant.name }}
+            </div>
+          </div>
+        </div>
+        <div class="p-2 grid grid-flow-row grid-cols-1 md:grid-cols-2">
+          <p class="py-2 text-sm text-gray-500">Options</p>
+          <div class="grid grid-flow-row grid-cols-3 gap-2">
+            <div
+              class="
+                border-2
+                p-3
+                align-middle
+                bg-gray-500
+                text-white
+                rounded-md
+                active:bg-gray-200
+                justify-center
+                flex
+                items-center
+              "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+            </div>
+            <div
+              v-for="option in item.options"
+              :key="option.key"
+              class="
+                border-2
+                p-3
+                align-middle
+                bg-gray-50
+                rounded-md
+                active:bg-gray-200
+                text-center
+              "
+            >
+              {{ option.name }}
+            </div>
+          </div>
+        </div>
+        <div class="p-2 grid grid-flow-row grid-cols-1 md:grid-cols-2">
+          <p class="py-2 text-sm text-gray-500">Sales Type</p>
+          <div class="grid grid-flow-row grid-cols-2 md:grid-cols-3 gap-2">
+            <div
+              class="
+                border-2
+                p-3
+                align-middle
+                bg-gray-500
+                text-white
+                rounded-md
+                active:bg-gray-200
+                justify-center
+                flex
+                items-center
+                shadow
+              "
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+            </div>
+            <div
+              v-for="sales_type in item.sales_types"
+              :key="sales_type.key"
+              class="
+                border-2
+                p-3
+                align-middle
+                bg-gray-50
+                rounded-md
+                active:bg-gray-200
+                text-center
+              "
+            >
+              <p>{{ sales_type.name }}</p>
+              <hr class="border-gray-400" />
+              <p>{{ currency(sales_type.price) }}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -34,6 +201,45 @@
         />
       </svg>
     </button>
+
+    <!-- Save Button -->
+    <button
+      @click="save()"
+      type="submit"
+      class="
+        absolute
+        bottom-4
+        right-4
+        z-20
+        flex
+        items-center
+        bg-modern-green
+        text-white
+        py-3
+        px-4
+        rounded
+        shadow-lg
+      "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 mr-2"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+        />
+      </svg>
+      SAVE
+    </button>
+
+    <!-- Modal Add Variant -->
+    <modal-add-variant @add-new-variant="addNewVariant" />
   </div>
 </template>
 
@@ -42,16 +248,24 @@ import moment from "moment";
 import "moment/locale/id";
 import * as numberFormat from "@/custom_package/number.js";
 import axios from "axios";
+import ModalAddVariant from "@/views/components/ModalAddVariant.vue";
 let apiHost = process.env.VUE_APP_BACKEND_HOST;
 
 export default {
   name: "EditItem",
-  components: {},
+  components: { ModalAddVariant },
 
   data() {
     return {
       user: JSON.parse(localStorage.getItem("user")),
       item: {},
+      categories: [
+        {
+          id: null,
+          store_id: null,
+          name: "",
+        },
+      ],
     };
   },
 
@@ -70,12 +284,52 @@ export default {
     },
     getItem() {
       this.item = JSON.parse(localStorage.getItem("itemSelected"));
-      localStorage.removeItem("itemSelected");
+      // localStorage.removeItem("itemSelected");
+    },
+    async fetchCategories() {
+      try {
+        let headers = { Authorization: `Bearer ${this.user.token}` };
+        const response = await axios.get(apiHost + "/api/store/categories", {
+          headers,
+          params: {
+            store_id: localStorage.getItem("selectedStore"),
+          },
+        });
+        this.categories = response.data.data;
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+
+    showAddNewVariant() {
+      let modal = document.getElementById("modal-add-variant");
+      modal.style.display = "block";
+    },
+    addNewVariant(variant) {
+      this.item.variants.push(variant);
+    },
+
+    async save() {
+      try {
+        let headers = { Authorization: `Bearer ${this.user.token}` };
+        const response = await axios.put(
+          apiHost + "/api/store/items",
+          this.item,
+          {
+            headers,
+          }
+        );
+        alert("berhasil edit");
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 
   mounted() {
     this.getItem();
+    this.fetchCategories();
   },
 };
 </script>
